2972bc3c04413d6b8a9aa910ee71c0e103fa6ae1
aakashshanavas <ashanavas@usf.edu>
2021-03-19 17:56:36 -0400

    added data page

--- js/main.js
+++ js/main.js
@@ -9,6 +9,31 @@ document.addEventListener('DOMContentLoaded', function() {
   var ulElement = document.getElementById('links');
   var liElement = document.createElement('li');
   liElement.appendChild(document.createTextNode('created by main.js'));
+  3/7/2021	1/1/2021	1/31/2021	By Month	Alabama	Respiratory diseases	Influenza and pneumonia	J09-J18	0-24	0
+3/7/2021	1/1/2021	1/31/2021	By Month	Alabama	Respiratory diseases	Influenza and pneumonia	J09-J18	35-44	16
+3/7/2021	1/1/2021	1/31/2021	By Month	Alabama	Respiratory diseases	Influenza and pneumonia	J09-J18	45-54	40
+3/7/2021	2/1/2021	2/28/2021	By Month	Alabama	Respiratory diseases	Influenza and pneumonia	J09-J18	45-54	13
+3/7/2021	1/1/2021	1/31/2021	By Month	Alabama	Respiratory diseases	Influenza and pneumonia	J09-J18	55-64	101
+3/7/2021	2/1/2021	2/28/2021	By Month	Alabama	Respiratory diseases	Influenza and pneumonia	J09-J18	55-64	41
+3/7/2021	1/1/2021	1/31/2021	By Month	Alabama	Respiratory diseases	Influenza and pneumonia	J09-J18	65-74	218
+3/7/2021	2/1/2021	2/28/2021	By Month	Alabama	Respiratory diseases	Influenza and pneumonia	J09-J18	65-74	67
+3/7/2021	1/1/2021	1/31/2021	By Month	Alabama	Respiratory diseases	Influenza and pneumonia	J09-J18	75-84	263
+3/7/2021	2/1/2021	2/28/2021	By Month	Alabama	Respiratory diseases	Influenza and pneumonia	J09-J18	75-84	70
+3/7/2021	1/1/2021	1/31/2021	By Month	Alabama	Respiratory diseases	Influenza and pneumonia	J09-J18	85+	155
+3/7/2021	2/1/2021	2/28/2021	By Month	Alabama	Respiratory diseases	Influenza and pneumonia	J09-J18	85+	28
+3/7/2021	1/1/2021	1/31/2021	By Month	Alabama	Respiratory diseases	Influenza and pneumonia	J09-J18	Not stated	0
+3/7/2021	2/1/2021	2/28/2021	By Month	Alabama	Respiratory diseases	Influenza and pneumonia	J09-J18	Not stated	0
+3/7/2021	1/1/2021	1/31/2021	By Month	Alabama	Respiratory diseases	Influenza and pneumonia	J09-J18	All Ages	795
+3/7/2021	2/1/2021	2/28/2021	By Month	Alabama	Respiratory diseases	Influenza and pneumonia	J09-J18	All Ages	226
+3/7/2021	1/1/2021	1/31/2021	By Month	Alaska	Respiratory diseases	Influenza and pneumonia	J09-J18	0-24	0
+3/7/2021	2/1/2021	2/28/2021	By Month	Alaska	Respiratory diseases	Influenza and pneumonia	J09-J18	0-24	0
+3/7/2021	1/1/2021	1/31/2021	By Month	Alaska	Respiratory diseases	Influenza and pneumonia	J09-J18	25-34	0
+3/7/2021	2/1/2021	2/28/2021	By Month	Alaska	Respiratory diseases	Influenza and pneumonia	J09-J18	25-34	0
+3/7/2021	1/1/2021	1/31/2021	By Month	Alaska	Respiratory diseases	Influenza and pneumonia	J09-J18	35-44	0
+3/7/2021	2/1/2021	2/28/2021	By Month	Alaska	Respiratory diseases	Influenza and pneumonia	J09-J18	35-44	0
+3/7/2021	2/1/2021	2/28/2021	By Month	Alaska	Respiratory diseases	Influenza and pneumonia	J09-J18	45-54	0
+3/7/2021	2/1/2021	2/28/2021	By Month	Alaska	Respiratory diseases	Influenza and pneumonia	J09-J18	75-84	0
+
 
   ulElement.appendChild(liElement);
 });
