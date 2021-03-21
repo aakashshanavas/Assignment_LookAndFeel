f7ef7cf921308ddd9db0b91a22e1ffa6c69d870b
Sivaramakrishna Allam <allams@usf.edu>
2021-03-21 17:51:51 -0400

    made about-us page center

--- js/main.js
+++ js/main.js
@@ -5,10 +5,14 @@
 /**
  * Sample code
  */
-document.addEventListener('DOMContentLoaded', function() {
-  var ulElement = document.getElementById('links');
-  var liElement = document.createElement('li');
-  liElement.appendChild(document.createTextNode('created by main.js'));
+// document.addEventListener('DOMContentLoaded', function() {
+//   var ulElement = document.getElementById('links');
+//   var liElement = document.createElement('li');
+//   liElement.appendChild(document.createTextNode('created by main.js'));
 
-  ulElement.appendChild(liElement);
-});
+//   ulElement.appendChild(liElement);
+// });
+
+function deleteAlert(){
+    alert("Are you sure you want to delete?");
+  }
